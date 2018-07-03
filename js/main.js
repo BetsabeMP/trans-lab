c1bdb119bfa43add95bbe6616e8291b3e063bc9a
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-07-03 19:55:19 -0400

    arreglos css, añadí otro js, arreglo funciones, navbar listo

--- js/main.js
+++ js/main.js
@@ -1,53 +1,15 @@
-//validar email
-const inputEmail = document.getElementById('inputEmail4');
-inputEmail.addEventListener('input', () => {
-    outback =  event.target;
-    mailValido = document.getElementById('emailOK');
-
-    emailCharac = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
-    if (emailCharac.test(outback.value)) {
-        mailValido.innerText = 'Correcto';
-    } else {
-        mailValido.innerText = 'Email incorrecto';
-    }
-});
-
-//contraseña -> aceptar max 8 caracteres y solo números
-const inputPassword = document.getElementById('inputPassword6');
-inputPassword.addEventListener('input', function() {
-    if (this.value.length > 8)
-    this.value = this.value.slice(0,8);
-});
-
-//imprimir opciones
-window.writeOption = (options) => {
-    let optionsHTML = '';
-    optionsHTML = optionsHTML + `<div class="pos-f-t">
-    <div class="collapse" id="navbarToggleExternalContent">
-      <div class="bg-dark p-4">
-        <h5 class="text-white h4">Collapsed content</h5>
-        <span class="text-muted">Toggleable via the navbar brand.</span>
-      </div>
-    </div>
-    <nav class="navbar navbar-dark bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </nav>
-  </div>`; 
-let divOptions = document.getElementById('navbar');
-divOptions.innerHTML = optionsHTML;
-
-};
+window.onload = () => {
+    window.validarEmail();
 
 //pasar a la sig sección
 const botonIniciarSesion = document.getElementById('botonIniciarSesion');
 botonIniciarSesion.addEventListener('click', () => {
-    window.writeOption();
-    console.log(writeOption);
+   
+        window.writeOption();
     
 });
+
+};
 
 
 /*
---
+++ js/view.js
@@ -0,0 +1,67 @@
+//imprimir opciones
+window.writeOption = (options) => {
+    let optionsHTML = '';
+    optionsHTML = optionsHTML + `<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="bg-dark p-4 optionsnav">
+        <div>Ver saldo</div>
+        <div>Calcular Tarifa</div>
+        <div>Perfil</div>
+        <div>Preguntas Frecuentes</div>
+        <div>Home</div>
+      </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navTitle">TRANS-LAB</div>
+    </nav>
+  </div>`; 
+    let divOptions = document.getElementById('navbar');
+    divOptions.innerHTML = optionsHTML;
+
+    let optionsHTMLrowDos = '';
+    optionsHTMLrowDos = optionsHTMLrowDos + `<div class="d-flex flex-sm-column optionsDos">
+    <p>VER SALDO</p>
+  </div>
+  <div class="d-flex flex-sm-column optionsDos">
+    <p>CALCULAR TARIFA</p> 
+  </div>
+  <div class="d-flex flex-sm-column optionsDos">
+    <p>PEFIL</p>
+  </div>
+  <div class="d-flex flex-sm-column optionsDos">
+    <p>PREGUNTAS FRECUENTES</p>
+  </div>`;
+    let divOptionsrowDos = document.getElementById('rowDos');
+    divOptionsrowDos.innerHTML = optionsHTMLrowDos;
+
+};
+
+
+//validar Email
+window.validarEmail = (email) => {
+    const inputEmail = document.getElementById('inputEmail4');
+    inputEmail.addEventListener('input', () => {
+        outback =  event.target;
+        mailValido = document.getElementById('emailOK');
+
+    emailCharac = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if (emailCharac.test(outback.value)) {
+        mailValido.innerText = 'Correcto';
+    } else {
+        mailValido.innerText = 'Email incorrecto';
+    }
+    });
+}
+
+//max 8 carácteres
+window.inputPass = (input) => {
+    const inputPassword = document.getElementById('inputPassword6');
+    inputPassword.addEventListener('input', function() {
+        if (this.value.length > 8)
+        this.value = this.value.slice(0,8);
+    });
+}
